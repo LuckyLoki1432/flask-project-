393ba9ada0d70c874650f104bbbb49e3ee5f6a39
ANDHAVARAPU LOKESH <126751857+LuckyLoki1432@users.noreply.github.com>
2024-04-12 00:33:50 +0000

    upto static page uploading was completd

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
+
+JOBS = [
+
+{
+
+'id': 1,
+
+'title': 'Data Analyst',
+
+'location': 'Bengaluru, India',
+
+'salary': 'Rs. 10,00,000'
+
+},
+{
+
+  'id': 2,
+
+  'title': 'Data Scientist',
+
+  'location': 'Delhi, India',
+
+  'salary': 'Rs. 15,00,000'
+
+},
+{
+
+  'id': 3,
+
+  'title': 'Frontend Engineer',
+
+  'location': 'San Francisco, USA',
+
+  'salary': '$120.00'
+
+},
+{
+
+  'id': 4,
+
+  'title': 'bACKEND eNGINEER',
+
+  'location': 'REMOTE',
+
+  'salary': 'Rs. 12,00,000'
+
+},
+
+]
+
 @app.route("/")
-def hello_world():
-  return "Hello,world"
+def hello_Lucky():
+  return render_template("home.html",jobs=JOBS)
 
 
 if __name__ == "__main__":
